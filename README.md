ca8f2f2c193b1e368478649028e8a322132604b7
rmscouto <rui.couto@di.uminho.pt>
2016-12-09 10:00:48 +0000

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # swing_jdbc_base
 Swing/JDBC workshop base project
+Now containing the workshop code.
--- src/pt/uminho/di/data/database/Connect.java
+++ src/pt/uminho/di/data/database/Connect.java
@@ -9,17 +9,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- * This class
+ * This class handles the connections to the database
  * @author ruicouto
  */
 public class Connect {
     
-    private static final String USERNAME = "your_mysql_username";
-    private static final String PASSWORD = "your_mysql_password";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
     private static final String URL = "localhost";
     private static final String SCHEMA = "albums";
     
-
+    /**
+     * Only required once
+     */
     static {
         try {
             Class.forName("com.mysql.jdbc.Driver");   
@@ -28,6 +30,11 @@ public class Connect {
         }
     }
     
+    /**
+     * Open a connection to the database. Don't forget to close-it after using it.
+     * @return
+     * @throws SQLException 
+     */
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection("jdbc:mysql://"+URL+"/"+SCHEMA+"?user="+USERNAME+"&password="+PASSWORD);
     }
